cfe7b2c8c09527dfe85106f5c03b73c1578618b7
Xiaoxing Hu <dawnstar.hu@gmail.com>
2019-10-17 13:53:05 +1300

    print out metadata

--- src/gatsby-theme-orga/components/post.js
+++ src/gatsby-theme-orga/components/post.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Layout from '../../components/layout'
+import { inspect } from 'util'
+const print = o => {
+  return inspect(o, false, null, false)
+}
 
 export default ({ post }) => {
   return (
     <Layout>
       <h1>{ post.metadata.title }</h1>
+      <h5>metadata:</h5>
+      <pre>{ print(post.metadata) }</pre>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
     </Layout>
   )
