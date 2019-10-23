1fd575e2079b6f622c1da681abef19881aa8c1ee
Xiaoxing Hu <xiaoxing@huxx.org>
2019-10-23 16:41:21 +1300

    better dummy content

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,14 +11,14 @@ module.exports = {
       options: {
         // basePath: `/blog`, // if you want your content to be on a prefix, default is `/`
         // contentPath: `blog`, // source of your org files, default to `content`
-        filter: { // filter of your posts, key should match metadata propperty names, default is {}
-          category: `posts`,
-        },
-        // pagination: 10 // default to 10
+        // filter: { // filter of your posts, key should match metadata propperty names, default is {}
+        //   category: `posts`,
+        // },
+        pagination: 5, // default to 10
         // slug: ['blog', '$export_file_name'], // how to generate slug for posts, default ['$category', '$export_file_name']
         // buildIndexPage: true, // default true
         // buildCategoryIndexPage: true, // default true
-        // metadata: ['title', 'category'], // pick the metadata you need in your custom `post` and `posts` components
+        metadata: ['title', 'category', 'date', 'tags'], // pick the metadata you need in your custom `post` and `posts` components
         // sortBy: [`date`], // sort posts
         // order: `DESC`, // order for sorting
       },
